02d85094cb9605414db8313691457bc427fb9777
wangning0 <wangning@wangningdeMacBook-Pro.local>
2017-07-06 20:59:45 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,33 +1,20 @@
-// import React from 'react';
-// import { render } from 'react-dom';
-// import './index.css';
-// import routes from './routes';
-// import store from './store';
-// import { Provider } from 'react-redux';
-// import { browserHistory } from 'react-router';
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-// import registerServiceWorker from './registerServiceWorker';
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+import routes from './routes';
+import store from './store';
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import registerServiceWorker from './registerServiceWorker';
 
-// window.init = function (data) {
-//     alert('data');
-//     console.log(data);
-//     store.dispatch({
-//         type: 'GET_UNSER_INFO',
-//         result: data
-//     })
-//     render(
-//         <MuiThemeProvider>
-//             <Provider store={store}>
-//                 {routes(browserHistory)}
-//             </Provider>
-//         </MuiThemeProvider>,
-//         document.getElementById('root')
-//     );
-//     registerServiceWorker();
-// }
 
-window.init = function(data) {
-    alert('data');
-    window.data = data;
-    require('./innerIndex')
-}
+render(
+    <MuiThemeProvider>
+        <Provider store={store}>
+            {routes(browserHistory)}
+        </Provider>
+    </MuiThemeProvider>,
+    document.getElementById('root')
+);
+registerServiceWorker();
--- src/reducer.js
+++ src/reducer.js
@@ -2,11 +2,11 @@ import {
     CHANGE_BUBBLES,
     GET_UNSER_INFO
 } from './actionTypes';
-//const $ = window.$; // eslint-disable-line
+const $ = window.$; // eslint-disable-line
 //const info = JSON.parse($.testinfo());
-//const info = $.testinfo();
+const info = $.testinfo();
 // console.log(info);
-const initialState = window.data; // eslint-disable-line
+const initialState = info; // eslint-disable-line
 
 /**
  * {"ret":200,"data":{"code":0,"msg":"login success","UserID":"7","numofBubbles":1},"msg":""}
