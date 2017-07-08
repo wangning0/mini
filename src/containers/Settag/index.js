982d86db7da4b00ea3b96468b74f344d9f755bca
wangning0 <wangning@wangningdeMacBook-Pro.local>
2017-07-08 22:53:57 +0800

    add

--- src/containers/Settag/index.js
+++ src/containers/Settag/index.js
@@ -74,7 +74,7 @@ class Answer extends Component {
                 <RaisedButton
                         label="提交" 
                         primary={true}
-                        style={{marginTop: '50px', width: '250px'}}
+                        style={{marginTop: '50px', width: '300px'}}
                         onClick={() => this.handleClick()}
                     />
             </div>
